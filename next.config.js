4bb2c8ab478365d379be48be8454d4a821492988
Jazu10 <jaseemshaoo4@gmail.com>
2021-10-14 16:21:37 +0530

    komba

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
    env: {
       BASE_URL: "http://topupstore.vercel.app/",
       MONGODB_URL:
-         "mongodb+srv://jaseem:jarvis007@cluster0.hj8dk.mongodb.net/elearn?retryWrites=true&w=majority",
+         "mongodb+srv://jaseem:jarvis008@cluster0.lqcju.mongodb.net/topupstore?retryWrites=true&w=majority",
       ACCESS_TOKEN_SECRET: "j76jxfmddJ/P:D[ZbhN-)hgG<bQBY,2SNS#7FFGu.<Ud>[':F7",
       REFRESH_TOKEN_SECRET:
          "V9W]q(&#VnHdxQ(KY-#`U}Uj42Bg7Y^^Q#!N=xC[vS_Q>>r)#_ZgFuFSp:MAxbPn8gA3]7!K=eQ{-PHU",
