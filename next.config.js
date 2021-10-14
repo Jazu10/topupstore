63d4498db6e71be8fd56712f80b2e7cba8b47db1
Jazu10 <jaseemshaoo4@gmail.com>
2021-10-14 15:15:29 +0530

    komba

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+   images: {
+      domains: [
+         "res.cloudinary.com",
+         "links.papareact.com",
+         "firebasestorage.googleapis.com",
+      ],
+   },
+   env: {
+      BASE_URL: "http://topupstore.vercel.app/",
+      MONGODB_URL:
+         "mongodb+srv://jaseem:jarvis008@cluster0.lqcju.mongodb.net/topupstore?retryWrites=true&w=majority",
+      ACCESS_TOKEN_SECRET: "j76jxfmddJ/P:D[ZbhN-)hgG<bQBY,2SNS#7FFGu.<Ud>[':F7",
+      REFRESH_TOKEN_SECRET:
+         "V9W]q(&#VnHdxQ(KY-#`U}Uj42Bg7Y^^Q#!N=xC[vS_Q>>r)#_ZgFuFSp:MAxbPn8gA3]7!K=eQ{-PHU",
+   },
+};
